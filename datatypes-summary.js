7d491dc5e61b19965c6fa702c34d8ff3ebe5a4e7
Abhishek9859 <abhishek7277329098>
2025-02-20 12:35:26 +0530

    Basics Of JavaScript

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -20,7 +20,7 @@ const bigNumber = 342346576324235453n;
 
 
 
-// Reference (NOn primitive)
+// Reference (Non-primitive)
 
 // Array , Objects , Functions
 
@@ -35,8 +35,47 @@ const myFunction = function(){
   console.log("Hello World")
 }
 
-console.log(typeof bigNumber)
-console.log(typeof myFunction);
-console.log(typeof myObj)
-console.log(typeof heros)
-console.log(typeof id )
+
+
+//console.log(typeof score)                      // => number
+//console.log(typeof scoreValue)                // => number
+//console.log(typeof outsideTemp)              // => object
+//console.log(typeof isLoggedIn)              // => boolean
+//console.log(typeof id )                    // =>  symbol
+//console.log(typeof anotherId)             // =>  symbol
+//console.log(typeof bigNumber)            // => bigint
+//console.log(typeof heros)               // => object
+//console.log(typeof myObj)              // =>  object
+//console.log(typeof myFunction);       // => function
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Meomory type => Stack and Heap 
+// Stack(Primitive)=> it used in primitive value             ,              Heap(Non-primitive)=>it is used in non-primitive value
+//      |                                                                            |
+// it takes copy of value                     it gives refernce of a value , whatever we commit to change is also changes in original value
+
+
+let myYoutubename = "abhishekdotcom";
+let anothername =  myYoutubename;
+
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne ={
+    email:"user@google.ccom",
+    upi : "user@abi"
+}
+
+let userTwo  = userOne
+
+userTwo.email = "abhishek@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
